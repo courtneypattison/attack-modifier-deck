b9894dfcd898d6f41667a24edf6b5fff19c3b5d1
courtneypattison <courtney@courtneypattison.com>
2018-10-17 19:30:19 -0700

    Added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAGSU2-MYijxeyuYBhokoKIsLYnB2QmHcI',
+    authDomain: 'attack-modifier-deck.firebaseapp.com',
+    databaseURL: 'https://attack-modifier-deck.firebaseio.com',
+    projectId: 'attack-modifier-deck',
+    storageBucket: 'attack-modifier-deck.appspot.com',
+    messagingSenderId: '469222736772'
+  }
 };
