c8398f4c424c9038d666c7efd4748e9d3af24af2
courtneypattison <courtney@courtneypattison.com>
2019-08-08 12:02:10 -0400

    Added auth to header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../../auth/shared/auth.service';
 
 @Component({
   selector: 'amd-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userInitial: Observable<string>;
 
+  constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+    this.userInitial = this.authService.getUserInitial();
+  }
+
+  signOut() {
+    this.authService.signOut();
+  }
 }
