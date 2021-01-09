e6fcc6dc5beca48d1fd92d44725cbc2b057a0531
courtneypattison <courtneyp@ttison.ca>
2021-01-09 14:46:36 -0500

    Use store to dispatch signout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
+import { signOut } from '../../auth/actions/auth.actions';
+import * as fromAuth from '../../auth/reducers';
 import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
@@ -12,13 +15,15 @@ import { AuthService } from '../../auth/services/auth.service';
 export class HeaderComponent implements OnInit {
   userInitial: Observable<string>;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private store: Store<fromAuth.State>) { }
 
   ngOnInit() {
     this.userInitial = this.authService.getUserInitial();
   }
 
   signOut() {
-    this.authService.signOut();
+    this.store.dispatch(
+      signOut()
+    );
   }
 }
